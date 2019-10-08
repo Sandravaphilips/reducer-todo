fbba64416631b3a8e4c0dc089dd7c3c3800eaf7f
Sandrava Philips <lacasandra1@gmail.com>
2019-10-08 14:16:18 +0100

    built a function to add todo

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -10,8 +10,27 @@ const initialTodo = [
 
 function reducer(state, action) {
     switch(action.type) {
+        case 'ADD_TODO':{
+            addTodo(e, todoTask, state);
+            break;
+        }
         default:
             return state;
+    }
+}
+
+function addTodo(e, todoTask, state) {
+    e.preventDefault();
+    const existing = state.filter(
+      todo => todo.task === todoTask
+    );
+    if (existing.length === 0) {
+      const newTodo = {
+        task: todoTask,
+        id: Date.now(),
+        completed: false
+      };
+      return [...state, newTodo]
     }
 }
 
